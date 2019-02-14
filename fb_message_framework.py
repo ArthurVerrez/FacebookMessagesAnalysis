57e843b420e4e4df506cc3b8169aadfa21e6e03c
Arthur Verrez <33450143+ArthurVerrez@users.noreply.github.com>
2019-02-14 20:06:11 +0100

    Added functionalities

--- fb_message_framework.py
+++ fb_message_framework.py
@@ -1,8 +1,10 @@
+
 from datetime import datetime
 import json
 import copy
 from matplotlib import pyplot as plt
-
+import matplotlib.image as mpimg
+import numpy as np
 
 class Conversation():
     path=""
@@ -49,7 +51,7 @@ class Conversation():
                     gifs.append(data["messages"][i]["gifs"][j]["uri"])
                     
             if("reactions" in data["messages"][i]):
-                has_gif=True
+                has_reaction=True
                 for j in range(len(data["messages"][i]["reactions"])):
                     reactions.append(Reaction(self.id_participant_name(data["messages"][i]["reactions"][j]["actor"]), data["messages"][i]["reactions"][j]["reaction"]))
             
@@ -77,7 +79,8 @@ class Conversation():
     def getMessagesDates(self):
         dates=[[] for k in range(len(self.participants))]
         for m in self.messages:
-            dates[m.sender.id].append(m.time)
+            if(m.sender!=None):
+                dates[m.sender.id].append(m.time)
         return dates
     
     #Return the dates of the messages as an array
@@ -86,7 +89,7 @@ class Conversation():
         for m in self.messages:
             dates.append(m.time)
         return dates
-            
+    
     
     #Show a graph with the cumulative number of messages for each participants over time
     def showGraphMessagesDates(self):
@@ -103,10 +106,31 @@ class Conversation():
         
         plt.show()
     
+    #Show the n participants' curves who have the most messages
+    def showGraphMessagesDatesMostImportant(self, n=5):
+        dates=self.getMessagesDates()
+        n_messages=np.array([len(dates[i]) for i in range(len(dates))])
+        sorted_id_n_messages=np.argsort(n_messages)[::-1][:n]
+        
+        dates_important=[dates[k] for k in sorted_id_n_messages]
+        
+        
+        
+        for i in range(len(dates_important)):
+            plt.plot_date(dates_important[i], [k for k in range(len(dates_important[i]))][::-1], label=self.participants[sorted_id_n_messages[i]].name, marker='None', linestyle="-")
+            
+        plt.title("Cumulative number of messages for each participants over time")
+        plt.xlabel('Date')
+        plt.ylabel('Cumulative number of messages')
+
+        plt.legend()
+        
+        plt.show()
+    
     def showGraphMessagesDatesTotal(self):
         dates=self.getMessagesDatesTotal()
         
-        plt.plot_date(dates[i], [k for k in range(len(dates))][::-1], label="Messages count", marker='None', linestyle="-")
+        plt.plot_date(dates, [k for k in range(len(dates))][::-1], label="Messages count", marker='None', linestyle="-")
             
         plt.title("Cumulative number of messages over time")
         plt.xlabel('Date')
@@ -115,7 +139,67 @@ class Conversation():
         plt.legend()
         
         plt.show()
+        
+        
+        
+    def mostReactedMessage(self):
+
+        message=self.messages[0]
+        
+        for m in self.messages:
+            if(m.has_reaction and len(m.reactions)>=len(message.reactions)):
+                message=m
+        return message
     
+    def listMostReactedMessages(self):
+        reacts=np.array([len(m.reactions) for m in self.messages])
+        most_reacted=np.argsort(reacts)[::-1]
+        return [self.messages[k] for k in most_reacted]
+    
+    def messagesAutoReact(self):
+        messages=[]
+        for m in self.messages:
+            if(m.has_reaction):
+                if(m.sender in [m.reactions[k].sender for k in range(len(m.reactions))]):
+                    messages.append(m)
+        return messages
+    
+    def reactsPerParticipant(self):
+        reacts=[0 for k in range(len(self.participants))]
+        for m in self.messages:
+            if(m.has_reaction):
+                for r in m.reactions:
+                    if(r.sender!=None):
+                        reacts[r.sender.id]+=1
+        return reacts
+    
+    def showGraphNumberOfReacts(self):
+        reacts=self.reactsPerParticipant()
+        fig, ax = plt.subplots()
+        
+        people = [self.participants[k].name for k in range(len(self.participants))]
+        y_pos = np.arange(len(people))
+        
+        
+        ax.barh(y_pos, reacts, align='center',
+                color='green')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(people)
+        ax.invert_yaxis()  # labels read top-to-bottom
+        ax.set_xlabel('Number of reactions for every participant')
+        ax.set_title('Number of reactions')
+    
+        plt.show()
+    
+        
+        
+    
+def displayPhoto(m):
+    path_total="C:/Users/Arthur Verrez/Desktop/messages/inbox/blueziquettepremium_4jsi_-62uw/photos/"
+    path="C:/Users/Arthur Verrez/Desktop/"
+    img=mpimg.imread(path+m.photos[0])
+    imgplot = plt.imshow(img)
+    plt.show() 
         
 class Person():
     name=""
@@ -156,4 +240,4 @@ class Reaction():
         self.sender=sender
         self.reaction=reaction
 
-conv = Conversation("C:/Users/Arthur Verrez/Drive/Projets/Programmation/Python/MessengerAnalysis/message.json")
+conv = Conversation("C:/Users/Arthur Verrez/Drive/Projets/Programmation/Python/MessengerStats/message3.json")
